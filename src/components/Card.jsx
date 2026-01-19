f43327308970bff89bfe5b04a7ae328c62a2bdbf
Taanveer22 <taanveer469@gmail.com>
2026-01-20 02:16:12 +0600

    alhamdulliah delete cofee from client

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,41 @@
-//  name,
-//       quantity,
-//       supplier,
-//       taste,
-//       category,
-//       details,
-//       photo,
+import Swal from "sweetalert2";
+
 const Card = ({ item }) => {
   // console.log(item);
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffees/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="max-w-3xl bg-[#f7f4ef] rounded-xl p-6 flex items-center gap-6">
       {/* Left: Image */}
       <div className="w-28 h-36 shrink-0">
         <img
-          src={item?.photo}
+          src={item?.photo || null}
           alt="Espresso Coffee"
           className="w-full h-full object-contain"
         />
@@ -24,7 +47,8 @@ const Card = ({ item }) => {
           <span className="font-semibold">Name:</span> {item?.name}
         </p>
         <p className="text-gray-800">
-          <span className="font-semibold">Supplier:</span>{item?.supplier}
+          <span className="font-semibold">Supplier:</span>
+          {item?.supplier}
         </p>
       </div>
 
@@ -38,7 +62,10 @@ const Card = ({ item }) => {
           ✏️
         </button>
 
-        <button className="w-9 h-9 rounded-md bg-red-500 text-white flex items-center justify-center">
+        <button
+          onClick={() => handleDelete(item._id)}
+          className="w-9 h-9 rounded-md bg-red-500 text-white flex items-center justify-center"
+        >
           🗑
         </button>
       </div>
