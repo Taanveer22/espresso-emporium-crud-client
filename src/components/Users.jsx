0f600021973bc519178e071207de5f66719a019e
Taanveer22 <taanveer469@gmail.com>
2026-01-21 06:18:42 +0600

    alhamdulliah delete user fully implemented in the client side

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,46 @@
 import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
   const [signedUsers, setSignedUsers] = useState(loadedUsers);
+
+  const handleDeleteUser = (id) => {
+    // console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // === DELETE FROM DATABASE ===
+        fetch(`http://localhost:5000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "User has been deleted.",
+                icon: "success",
+              });
+            }
+
+            const remainingSignedUsers = signedUsers.filter(
+              (element) => element._id !== id,
+            );
+            setSignedUsers(remainingSignedUsers);
+          });
+      }
+    });
+  };
   return (
     <div>
       <h1 className="text-3xl font-medium text-center my-5">
@@ -31,7 +68,10 @@ const Users = () => {
                   <td>{element.creationTime}</td>
                   <td>
                     <button className="btn btn-xs btn-info">Edit</button>
-                    <button className="btn btn-xs btn-error ml-5">
+                    <button
+                      onClick={() => handleDeleteUser(element._id)}
+                      className="btn btn-xs btn-error ml-5"
+                    >
                       Delete
                     </button>
                   </td>
